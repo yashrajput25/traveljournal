5084d63aba35b44f7075654a0dd8bacf6eb17b88
Yash Rajput <yashrajputyr10@gmail.com>
2024-12-02 13:11:10 +0530

    Created a header component

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import './component.css';
+
+export default function Header(){
+
+    return (
+        <header className="header">
+            <img className= "globe" src="./media/globe.png" alt="" />
+            <h1>my travel journal.</h1>
+        </header>
+    );
+
+}
