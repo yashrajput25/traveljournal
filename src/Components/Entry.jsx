bbeb5923f8af63598ae002c8a24b24c1fc79a471
Yash Rajput <yashrajputyr10@gmail.com>
2024-12-03 18:31:57 +0530

    Created an entry and made desired changes

---
+++ src/Components/Entry.jsx
@@ -0,0 +1,19 @@
+
+export default function Entry(){
+
+    return(
+        <article className="journal-entry">
+            <div className="main-image-container">
+                <img className="main-image" src="./media/mount_fuji.webp" alt="error" />
+                </div>
+           
+            <div className="text-container">
+                <img src="" alt="" />
+                <span>Japan</span>
+                <a href="https://maps.app.goo.gl/6DK8BpbrA7uGepzi9">View on Google Maps</a>
+                <h2>Mount Fuji</h2>
+                <p>12 Jan, 2021 - 24 Jan, 2021</p> <p>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+            </div>
+        </article>
+    );
+}
